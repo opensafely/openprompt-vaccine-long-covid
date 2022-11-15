bf9e7d0558d8fac1067528cca6e817e24a09047a
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2022-11-15 19:23:13 +0000

    update cohort_extractor version

--- analysis/report.R
+++ analysis/report.R
@@ -1,11 +1,16 @@
 library('tidyverse')
-
+df_dummy <- read_csv(here::here("output/dummy_data.csv"))
 df_input <- read_csv(
-  here::here("output", "input.csv.gz"),
-  col_types = cols(patient_id = col_integer(),age = col_double())
+  here::here("output", "dataset.csv"),
+  col_types = cols(patient_id = col_integer(),year_of_birth = col_double())
 )
 
-plot_age <- ggplot(data=df_input, aes(df_input$age)) + geom_histogram()
+df_input_select <- df_input %>% 
+  filter(prefup_4mo == 1)
+
+plot_age <- ggplot(data=df_input, aes(df_input$age, fill=factor(sex))) + 
+  geom_histogram() + 
+  theme_classic()
 
 ggsave(
   plot= plot_age,
--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -10,11 +10,22 @@ study = StudyDefinition(
     population=patients.registered_with_one_practice_between(
         "2019-02-01", "2022-08-01"
     ),
+    prefup_4mo=patients.with_complete_gp_consultation_history_between(
+      start_date = "2018-11-01", 
+      end_date = "2019-01-31",
+      return_expectations={"incidence": 0.96}
+    ),
     age=patients.age_as_of(
         "2019-09-01",
         return_expectations={
             "rate": "universal",
             "int": {"distribution": "population_ages"},
+        },
+    ),
+    sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"female": 0.51, "male": 0.49}},
         },
     ),
 )   
