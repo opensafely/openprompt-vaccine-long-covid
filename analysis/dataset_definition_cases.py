687fe8714113309f15dabfca24f26a304939a179
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2022-12-14 09:28:11 +0000

    restructure .yaml to run cases/controls

--- analysis/dataset_definition_cases.py
+++ analysis/dataset_definition_cases.py
@@ -3,7 +3,7 @@ from databuilder.tables.beta.tpp import (
   clinical_events
 )
 
-from analysis.variable_lib import long_covid_events_during
+from variable_lib import long_covid_events_during
 from datasets import add_common_variables, study_start_date, study_end_date
 
 dataset = Dataset()
@@ -13,8 +13,8 @@ lc_dx = long_covid_events_during(study_start_date, study_end_date)
 # long covid code
 first_lc_dx = lc_dx.sort_by(clinical_events.date).first_for_patient()
 
-add_common_variables(dataset, study_start_date, first_lc_dx.date, population=lc_dx.exists_for_patient())
+add_common_variables(dataset, study_start_date, first_lc_dx.date, population=first_lc_dx.exists_for_patient())
 
 # add specfic variables
-
 dataset.first_lc_dx = first_lc_dx.date
+dataset.test_to_lc_dx_gap = first_lc_dx.date - dataset.latest_test_before_diagnosis
