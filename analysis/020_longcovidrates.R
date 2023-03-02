5443aac3b456b55c263dd7820163989eb6fd3cea
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2023-03-02 11:42:47 +0000

    Use %--% intervals for date subtraction

--- analysis/020_longcovidrates.R
+++ analysis/020_longcovidrates.R
@@ -3,28 +3,29 @@
 
 library(tidyverse)
 library(survival)
+library(lubridate)
 library(here)
 
-cases <- readr::read_csv(here("output/dataset_cases.csv.gz")) %>% janitor::clean_names()
+cases <- readr::read_csv(here("output/dataset_cases.csv.gz")) %>% 
+  janitor::clean_names()
 spec(cases) %>% print()
-controls <- readr::read_csv(here("output/dataset_controls.csv.gz")) %>% janitor::clean_names()
+
+controls <- readr::read_csv(here("output/dataset_controls.csv.gz")) %>% 
+  janitor::clean_names()
 spec(controls) %>% print()
 
 df_full <- bind_rows(cases, 
                     controls) %>% 
   arrange(patient_id)
 
-#glimpse(df_full)
-#skimr::skim(df_full)
-
-
 # calculate the raw rate as a sense check ---------------------------------
 # using vaccine status at endpoint so obviously inaccurate as someone with 3 vaccines just before their LC record
 # did not spend the whole follow up with 3 vaccine doses. But we will deal with that in the next step...
 df_full <- df_full %>% 
-  mutate(t = as.numeric((pt_end_date - pt_start_date)/365.25),
+  mutate(t =  pt_start_date %--% pt_end_date / dyears(1),
          lc_out = as.numeric(!is.na(first_lc_dx)),
-         last_vacc_gap = last_vacc_gap/365.25) 
+         last_vacc_gap = last_vacc_gap / 365.25) 
+
 df_full %>% 
   group_by(no_prev_vacc) %>% 
   summarise(
@@ -40,15 +41,15 @@ df_full %>%
 
 # time update vaccine status ----------------------------------------------
 df_vacc_base <- df_full %>% 
-  filter(t>0)
+  filter(t>0) ## remove anyone who starts and ends on the same date 
 
 df_vacc_long <- df_full %>% 
   filter(t>0) %>% 
   dplyr::select(patient_id, pt_start_date, starts_with("covid_vacc_")) %>% 
   dplyr::select(!contains("adm")) %>%  ## ignoring vaccine records from admin records and only using vacc table results for now
   pivot_longer(cols = starts_with("covid_vacc_"), names_to = "vacc_no", names_patter = "covid_vacc_(.)_vacc_tab", values_to = "date") %>% 
-  mutate(t = as.numeric((date- pt_start_date)/365.25))
-df_vacc_long$t[df_vacc_long$t < 0] <- 0
+  mutate(t = pt_start_date %--% date / dyears(1))
+df_vacc_long$t[df_vacc_long$t < 0] <- 0 # if this gap is negative then the vaccine was delivered pre-study enrol so set t = 0
 
 df_vacc_timeupdated <- df_vacc_base %>% 
   survival::tmerge(df_vacc_base, id = patient_id, lc_out = event(t, lc_out)) %>% 
@@ -66,4 +67,3 @@ df_vacc_timeupdated %>%
   ) %>% 
   mutate(rate_per1e5 = (lc/fup)*100000) %>% 
   write_csv(here("output/tab021_tdc_lc_rates.csv"))
-
