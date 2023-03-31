d4965f1d4ce40451716ec42e0de1eba07ac97e6c
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2023-03-31 16:30:02 +0100

    Update 020_cruderates.R

--- analysis/020_cruderates.R
+++ analysis/020_cruderates.R
@@ -4,23 +4,18 @@ library(lubridate)
 library(arrow)
 library(here)
 
+source(here::here("analysis/functions/redaction.R"))
+source(here::here("analysis/functions/ggplot_theme.R"))
 source(here::here("analysis/functions/calculate_rates.R"))
 source(here::here("analysis/functions/time_update_vaccinedoses.R"))
 
+# create folder to put plot in
+dir.create(here("output/supplementary/"), showWarnings = FALSE, recursive=TRUE)
+
 # import, combine, clean data -------------------------------------------------
 cleaned_data <- arrow::read_parquet(here::here("output/clean_dataset.gz.parquet"))
-time_data <- arrow::read_parquet(here::here("output/timeupdate_dataset.gz.parquet"))
 
 # calculate crude rates ---------------------------------------------------
 crude_rates <- apply_rates_over_stratifiers(cleaned_data)
 crude_rates %>% 
   write_csv(here("output/tab021_crude_lc_rates.csv"))
-
-# time update on vaccine date ---------------------------------------------
-time_updated_rates <- bind_rows(
-  calculate_rates(time_data, vaccines),
-  calculate_rates(time_data, t_mixmatch)
-)
-
-time_updated_rates %>% 
-  write_csv(here("output/tab022_tuv_lc_rates.csv"))
