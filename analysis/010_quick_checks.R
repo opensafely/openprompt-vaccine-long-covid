ddf04d26bcfa10b7697db0e2a88d3dd7322ca80f
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2023-04-14 14:12:36 +0100

    delete old code

--- analysis/010_quick_checks.R
+++
@@ -1,4 +0,0 @@
-rmarkdown::render(input = here::here("analysis/010_quick_checks.Rmd"),
-                  output_format = "html_document",
-                  output_file = here::here("output/010_quick_checks.html")
-)
