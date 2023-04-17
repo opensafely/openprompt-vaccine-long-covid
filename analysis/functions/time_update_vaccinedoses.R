b91410aaeb41c57e54a76d3d13e963abf7527e83
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2023-04-17 11:54:48 +0100

    tmerge on the same patient_ids

--- analysis/functions/time_update_vaccinedoses.R
+++ analysis/functions/time_update_vaccinedoses.R
@@ -6,20 +6,30 @@
 
 time_update_vaccinedoses <- function(data, outcome_var){
   # remove anyone who starts and ends on the same date 
-  # need to redefine the participant end date because the outcome date may be different to Any Long COVID date (used in data builder)
+  # need to redefine the participant end date because the outcome date may be different to Any Long COVID date (used in data builder) 
+  #e.g., may have a fracture before/after Long COVID so need to update end date
   
   # Ran into strange error with dates when doing all-in-one (end dates in the year 4,562,122 etc.) 
   # so split it out into cases and non-cases
   small_base <- data %>% 
-    dplyr::select(patient_id, starts_with("pt_"), contains("age"), sex, {{outcome_var}}) %>% 
+    dplyr::select(patient_id, 
+                  starts_with("pt_"), 
+                  contains("age"), 
+                  sex, 
+                  {{outcome_var}}, 
+                  contains("vaccine_dose"),
+                  contains("vaccine_schedule")) %>% 
     rename(outcome = {{outcome_var}}) %>% 
     mutate(outcome_binary = as.numeric(!is.na(outcome)))
+  
+  # save some memory space
+  data <- NULL 
+  
   small_base_cases <- small_base %>% 
     filter(outcome_binary == 1) %>%
     # create new end date = outcome date (this may be the same as the current end date but it may be earlier or later depending on the outcome, so need to override)
     mutate(new_end_date = outcome,
-      t = pt_start_date %--% new_end_date / dyears(1)
-    )  %>% 
+           t = pt_start_date %--% new_end_date / dyears(1))  %>% 
     # again, need to filter out anyone who has the event before/on the same day as study entry
     filter(t>0)
   
@@ -27,14 +37,20 @@ time_update_vaccinedoses <- function(data, outcome_var){
   small_base_controls <- small_base %>% 
     filter(outcome_binary == 0) %>% 
     mutate(new_end_date = pt_end_date,
-           t = pt_start_date %--% new_end_date / dyears(1))
+           t = pt_start_date %--% new_end_date / dyears(1)) %>% 
+    # again, need to filter out anyone who has the event before/on the same day as study entry
+    filter(t>0)
   
   small_base <- bind_rows(small_base_cases, small_base_controls)
   
+  # save memory
+  small_base_cases <- NULL
+  small_base_controls <- NULL
+  
   # calculate time difference for each vaccine dose
-  df_vacc_base_t <- data %>%
+  df_vacc_base_t <- small_base %>%
+    # again, need to filter out anyone who has the event before/on the same day as study entry (should be redundant sbut just in case)
     filter(t>0) %>% 
-    #filter(patient_id %in% small_base$patient_id) %>% 
     select(patient_id, pt_start_date, contains("vaccine_dose_"), contains("vaccine_schedule_")) %>% 
     mutate(vacc_time_1 = pt_start_date %--% vaccine_dose_1_date / dyears(1),
            vacc_time_2 = pt_start_date %--% vaccine_dose_2_date / dyears(1),
@@ -42,7 +58,14 @@ time_update_vaccinedoses <- function(data, outcome_var){
            ) %>% 
     dplyr::select(-contains("vaccine_dose")) %>% 
     pivot_longer(cols = starts_with("vacc_time_"), names_to = "vacc_no", names_pattern = "vacc_time_(.)", values_to = "years")
-  data <- NULL
+  
+  ## check the right number of patids are involved before doing the computationally intensive tmerge stuff
+  isEmpty <- function(x) {
+    return(length(x)==0)
+  }
+  l1 <- unique(small_base$patient_id)
+  l2 <- unique(df_vacc_base_t$patient_id)
+  if(!isEmpty(setdiff(l1, l2))){stop("id values in time updated vaccine data that are not in base data")}
   
   # create new time-updated version of homologous/heterologous vaccine dose - need two versions of a time-updated variable (one with detail, one grouped by homo/hetero)
   vaccines_schedule_timeupdate <- df_vacc_base_t %>% 
@@ -53,18 +76,22 @@ time_update_vaccinedoses <- function(data, outcome_var){
   
   # create the survival dataset 
   survivaldata <- survival::tmerge(small_base, small_base, id = patient_id, out = event(t, outcome_binary)) 
-  newsurvival <- survival::tmerge(survivaldata, df_vacc_base_t, id = patient_id, vaccines = tdc(years, vacc_no, 0)) 
+  newsurvival <- survival::tmerge(survivaldata, vaccines_schedule_timeupdate, id = patient_id, vaccines = tdc(years, vacc_no, 0)) 
   
   newsurvival <- survival::tmerge(newsurvival, vaccines_schedule_timeupdate, id = patient_id, t_vacc_detail = tdc(years, vaccine_schedule_detail, "No vaccine")) 
   
-  newsurvival %>% 
-    select(-outcome_binary, -pt_end_date) %>% 
-    rename(pt_end_date = new_end_date) %>% 
-    mutate(vaccines = factor(vaccines,
-                             levels = 0:3, 
-                             labels = c("0","1","2","3+"))) %>% 
-    mutate(t_vacc_detail = factor(t_vacc_detail, 
-                               levels = c("No vaccine", levels(vaccines_schedule_timeupdate$vaccine_schedule_detail)))
-    ) %>% 
+  newsurvival %>%
+    select(-outcome_binary,-pt_end_date) %>%
+    rename(pt_end_date = new_end_date) %>%
+    mutate(vaccines = factor(
+      vaccines,
+      levels = 0:3,
+      labels = c("0", "1", "2", "3+")
+    )) %>%
+    mutate(t_vacc_detail = factor(t_vacc_detail,
+                                  levels = c(
+                                    "No vaccine",
+                                    levels(vaccines_schedule_timeupdate$vaccine_schedule_detail)
+                                  ))) %>%
     mutate(t = tstop - tstart)
 }
