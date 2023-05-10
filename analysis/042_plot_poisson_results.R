88c45049a544c1e4fd1924454ccc080321dfc85f
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2023-05-10 17:28:00 +0100

    change forest plots

--- analysis/042_plot_poisson_results.R
+++ analysis/042_plot_poisson_results.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(broom)
 library(lubridate)
+library(survival)
 library(here)
 
 source(here::here("analysis/functions/redaction.R"))
@@ -26,12 +27,182 @@ if(max(adjusted_rates_out$std_error, na.rm = T) > 1){
   adjusted_rates_out <- adjusted_rates_out %>% 
     filter(std_error<1)
 }
+
 outcome_list <- c("All Long COVID", "Long COVID diagnoses", "Fractures")
 cols <- hcl.colors(20, palette = "Viridis")[c(1,10,18)]
 names(cols) <- outcome_list
 
-# plot adjusted rates -----------------------------------------------------
-pdf(here("output/figures/fig3a_rate_ratios.pdf"), width = 12, height = 14)
+# plot rate ratios with table -----------------------------------------------------
+# bit of formatting so it all looks pretty in the plots
+full_rates <- adjusted_rates_out %>% 
+  # just keep the useful data
+  dplyr::select(strat_var, term2, rate, conf.low, conf.high, outcome, model, plot_marker, baseline, model) %>% 
+  # convert the lovely estimates to lovely strings
+  mutate(textrate = sprintf("%0.1f", round(rate, digits = 1)),
+         textci = sprintf(paste0("(", round(conf.low, 1), "-", round(conf.high, 1), ")"))) %>% 
+  # get rid of the variable names as part of the term2 label
+  mutate(term2 = str_remove_all(term2, "sex|age_cat|vaccines|ethnicity|comorbidities|practice_nuts|highrisk_shield|t_vacc_detail")) %>% 
+  mutate_at(c("term2"), ~ifelse(str_detect(term2, "baseline"), paste0(".", term2), .)) %>% 
+  # change ordering of outcome variable 
+  mutate(outcome = factor(outcome, levels = c("All Long COVID", "Long COVID diagnoses", "Fractures"))) %>% 
+  # change ordering of stratification variables
+  mutate(strat_var = factor(
+    strat_var,
+    levels = c(
+      "Age category",
+      "Sex",
+      "No. vaccine doses",
+      "Vaccine schedule (detail)",
+      "Region",
+      "Ethnicity",
+      "Comorbidities",
+      "Shielding (high risk group)"
+    )
+  )) %>% 
+  # filter so just the stratifier results are shown (not the age, and sex coefficients)
+  filter(plot_marker) 
+
+suppress_labs <- function(string) {
+  rep("", length(string))
+}
+
+create_forest_plot <- function(data_in, y_col_var, plot_rel_widths = c(6, 4)) {
+  if (y_col_var == "outcome"){
+    outcome_list <- c("All Long COVID", "Long COVID diagnoses", "Fractures")
+    cols <- hcl.colors(20, palette = "Viridis")[c(1,10,18)]
+    names(cols) <- outcome_list
+  } else {
+    n_cols <- dplyr::select(data_in, {{y_col_var}}) %>% unique() %>% pull()
+    cols <- hcl.colors(length(n_cols), palette = "Viridis")
+    names(cols) <- n_cols
+  }
+  
+  data_for_table <- data_in %>%
+    # keep the texty stuff for a nice table
+    dplyr::select(strat_var, term2, outcome, model, textrate, textci) %>%
+    # convert to longer for plotting
+    tidyr::pivot_longer(c(textrate, textci), names_to = "stat") %>%
+    mutate(stat = factor(stat, levels = c("textrate", "textci")))
+  
+  rr_tab <- data_for_table %>%
+    ggplot(aes(stat, term2, label = value)) +
+    geom_text(size = 4, hjust = 1) +
+    scale_x_discrete(position = "bottom", labels = c("RR", "95% CI")) +
+    facet_grid(
+      strat_var + term2 ~ get(y_col_var),
+      scales = "free_y",
+      space = "free_y",
+      switch = "y"
+    ) +
+    labs(x = NULL, y = NULL) +
+    theme_classic() +
+    theme(
+      strip.background = element_blank(),
+      strip.text.y.left = element_text(angle = 0, hjust = 0, vjust = 0.5, face = "bold"),
+      strip.text.x = element_text(face = "bold"),
+      panel.grid.major = element_blank(),
+      panel.border = element_blank(),
+      axis.line = element_blank(),
+      axis.text.y = element_blank(),
+      axis.text.x = element_text(size = 12, face = "bold", hjust = 1),
+      axis.ticks = element_blank(),
+      axis.title = element_text(face = "bold")
+      )
+  
+  pd <- position_dodge(2)
+  
+  rr_forest <- ggplot(data = data_in,
+                      aes(
+                        x = term2,
+                        y = rate,
+                        ymin = conf.low,
+                        ymax = conf.high,
+                        group = get(y_col_var),
+                        colour = get(y_col_var)
+                      )) +
+    geom_hline(yintercept = 1, colour = "gray60") +
+    #geom_errorbar(
+    #  data = data_plot,
+    #  position = pd,
+    #  width = 0,
+    #  cex = 1
+    #) +
+    geom_pointrange(position = pd) +
+    xlab("") +
+    ylab("Rate ratio (95% Confidence Interval)") +
+    facet_grid(
+      strat_var + term2 ~ " ",
+      scales = "free",
+      space = "free",
+      labeller = labeller(strat_var = suppress_labs,
+                          term2 = suppress_labs)
+    ) +
+    scale_color_manual(values = cols) +
+    scale_y_log10(breaks = c(0.25, 0.5, 1.0, 2.0, 4.0),
+                  limits = c(1/4.5, 4.5),
+                  minor_breaks = NULL) +
+    theme_classic() +
+    theme(
+      panel.background = element_blank(),
+      strip.background = element_rect(colour = NA, fill = NA),
+      strip.text.x = element_text(face = "bold", size = 12),
+      #strip.text.y = element_blank(),
+      panel.border = element_rect(fill = NA, color = "black"),
+      legend.position = "none",
+      axis.text.x = element_text(face = "bold"),
+      axis.text.y = element_blank(),
+      axis.title = element_text(size = 10, face = "bold"),
+      plot.title = element_text(
+        face = "bold",
+        hjust = 0.5,
+        size = 13
+      )
+    ) +
+    coord_flip()
+
+  cowplot::plot_grid(rr_tab, rr_forest, rel_widths = plot_rel_widths)
+}
+
+# Crude RRs
+pdf(here("output/figures/fig3a_crude_RRs.pdf"), width = 20, height = 20)
+  create_forest_plot(filter(full_rates, model == "crude"), y_col_var = "outcome")
+dev.off()
+
+# Adjusted RRs
+pdf(here("output/figures/fig3b_adjusted_RRs.pdf"), width = 20, height = 20)
+  create_forest_plot(filter(full_rates, model == "adjusted"), y_col_var = "outcome")
+dev.off()
+
+# Long COVID adjusted outcomes only
+pdf(here("output/figures/fig3c_longcovid_RRs.pdf"), width = 20, height = 20)
+  create_forest_plot(filter(full_rates, str_detect(outcome, "Long") & model == "adjusted"), y_col_var = "outcome")
+dev.off()
+
+# All Long COVID only: adjusted versus crude
+pdf(here("output/figures/fig3d_longcovid_models.pdf"), width = 20, height = 20)
+  create_forest_plot(filter(full_rates, outcome == "All Long COVID"), y_col_var = "model")
+dev.off()
+
+# Focus on vaccines
+pdf(here("output/figures/fig3e_vaccines.pdf"), width = 16, height = 8)
+  create_forest_plot(filter(full_rates, str_detect(strat_var, "accine") & model == "adjusted"), y_col_var = "outcome",
+                     plot_rel_widths = c(7.5, 3.5))
+dev.off()
+
+# vaccines and long covid focuse
+pdf(here("output/figures/fig3f_longcovid_vaccine_models.pdf"), width = 16, height = 8)
+  create_forest_plot(filter(full_rates, str_detect(outcome, "Long") & str_detect(strat_var, "accine") & model == "adjusted"), y_col_var = "outcome",
+                     plot_rel_widths = c(7, 3))
+dev.off()
+
+# non Vaccine stratifiers
+pdf(here("output/figures/fig3g_demographics.pdf"), width = 16, height = 12)
+  create_forest_plot(filter(full_rates, !str_detect(strat_var, "accine") & model == "crude"), y_col_var = "outcome",
+                   plot_rel_widths = c(7, 3))
+dev.off()
+
+# put them all on one plot  -----------------------------------------------
+pdf(here("output/figures/fig3h_rate_ratios_facet.pdf"), width = 12, height = 14)
 pd = position_dodge(1)
 adjusted_rates_out %>% 
   filter(plot_marker) %>% 
@@ -44,63 +215,6 @@ adjusted_rates_out %>%
   labs(y = "Incidence Rate Ratio (95% CI)", x = "") +
   scale_color_manual(outcome_list, values = cols) +
   scale_alpha_manual(c("crude", "adjusted"), values = c(0.5, 1)) +
-  guides(alpha = "none", colour = guide_legend("Outcome"), lty = guide_legend("Model")) +
-  theme_ali() +
-  theme(legend.position = "top",
-        strip.background = element_blank())
-dev.off()
-
-pdf(here("output/figures/fig3b_rate_ratios_longcovid.pdf"), width = 12, height = 14)
-pd = position_dodge(1)
-adjusted_rates_out %>% 
-  filter(plot_marker) %>% 
-  filter(outcome != "Fractures") %>% 
-  ggplot(aes(x=term2, y = rate, ymin = conf.low, ymax = conf.high, colour = outcome, lty = model)) +
-  geom_point(size = 1.5, pch = 16, position = pd) +
-  geom_linerange(lwd = 0.75, position = pd) +
-  geom_hline(yintercept = 1, lty = 2) +  # add a dotted line at x=1 after flip
-  coord_flip() +
-  facet_wrap(~strat_var, scales = "free", ncol = 3) + 
-  labs(y = "Incidence Rate Ratio (95% CI)", x = "") +
-  guides(alpha = "none", colour = guide_legend("Outcome"), lty = guide_legend("Model")) +
-  scale_color_manual(outcome_list, values = cols) +
-  theme_ali() +
-  theme(legend.position = "top",
-        strip.background = element_blank())
-dev.off()
-
-pdf(here("output/figures/fig3c_rate_ratios_adjustedonly.pdf"), width = 12, height = 14)
-pd = position_dodge(1)
-adjusted_rates_out %>% 
-  filter(plot_marker) %>% 
-  filter(model == "adjusted") %>% 
-  ggplot(aes(x=term2, y = rate, ymin = conf.low, ymax = conf.high, colour = outcome, lty = model)) +
-  geom_point(size = 1.5, pch = 16, position = pd) +
-  geom_linerange(lwd = 0.75, position = pd) +
-  geom_hline(yintercept = 1, lty = 2) +  # add a dotted line at x=1 after flip
-  coord_flip() +
-  facet_wrap(~strat_var, scales = "free", ncol = 3) + 
-  labs(y = "Incidence Rate Ratio (95% CI)", x = "") +
-  scale_color_manual(outcome_list, values = cols) +
-  guides(alpha = "none", colour = guide_legend("Outcome"), lty = guide_legend("Model")) +
-  theme_ali() +
-  theme(legend.position = "top",
-        strip.background = element_blank())
-dev.off()
-
-pdf(here("output/figures/fig3d_rate_ratios_controloutcome.pdf"), width = 12, height = 14)
-pd = position_dodge(1)
-adjusted_rates_out %>% 
-  filter(plot_marker) %>% 
-  filter(outcome == "Fractures") %>% 
-  ggplot(aes(x=term2, y = rate, ymin = conf.low, ymax = conf.high, colour = outcome, lty = model)) +
-  geom_point(size = 1.5, pch = 16, position = pd) +
-  geom_linerange(lwd = 1, alpha = 0.8, position = pd) +
-  geom_hline(yintercept = 1, lty = 2) +  # add a dotted line at x=1 after flip
-  coord_flip() +
-  facet_wrap(~strat_var, scales = "free", ncol = 3) + 
-  labs(y = "Incidence Rate Ratio (95% CI)", x = "") +
-  scale_color_manual(outcome_list, values = cols) +
   guides(alpha = "none", colour = guide_legend("Outcome"), lty = guide_legend("Model")) +
   theme_ali() +
   theme(legend.position = "top",
