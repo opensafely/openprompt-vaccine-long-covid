2ee1e810304d69b47575e71d06d113f14e50f06e
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2023-05-25 15:55:43 +0100

    create a table 2 by long covid status

--- analysis/013_create_table1.R
+++ analysis/013_create_table1.R
@@ -4,6 +4,7 @@ library(glue)
 library(arrow)
 library(gtsummary)
 library(lubridate)
+library(forcats)
 
 source(here::here("analysis/functions/ggplot_theme.R"))
 source(here::here("analysis/functions/redaction.R"))
@@ -17,14 +18,6 @@ fs::dir_create(output_dir_tab)
 output_dir_plots <- here("output/supplementary")
 fs::dir_create(output_dir_plots)
 
-
-# redaction functions for data --------------------------------------------
-roundmid_any <- function(x, to=1){
-  # like ceiling_any, but centers on (integer) midpoint of the rounding points
-  ceiling(x/to)*to - (floor(to/2)*(x!=0))
-}
-
-# will update this when releasing outputs
 threshold <- 7
 
 # import data -------------------------------------------------------------
@@ -49,8 +42,9 @@ baseline_data <- cleaned_data %>%
   dplyr::mutate(pt_start_year = factor(year(pt_start_date))) %>% 
   dplyr::select(-pt_start_date,
                 -pt_end_date) %>% 
-  dplyr::select(pt_start_year, everything())
-
+  dplyr::select(pt_start_year, everything()) %>% 
+  # use forcats package to create an explicit "missing" value 
+  mutate_if(is.factor, ~forcats::fct_explicit_na(.))
 
 # var_labels --------------------------------------------------------------
 var_labels <- list(
@@ -66,11 +60,12 @@ var_labels <- list(
   comorbidities ~ "Comorbidities",
   care_home ~ "Resident in care home",
   highrisk_shield ~ "Shielding (high risk group)",
-  lowrisk_shield ~ " Shielding (Low/moderate risk group)"
+  lowrisk_shield ~ "Shielding (Low/moderate risk group)"
 )
 
 var_labels_full <- splice(
   var_labels,
+  no_prev_vacc ~ "Number of vaccines received", 
   lc_out ~ "Any Long COVID record", 
   lc_dx_only ~ "Long COVID diagnosis",
   lc_cat ~ "Long COVID record by prior status",
@@ -88,107 +83,157 @@ var_labels_full <- var_labels_full %>%
   set_names(., map_chr(., all.vars))
 
 # make table 1 ------------------------------------------------------------
-table1 <- baseline_data %>% 
-  dplyr::select(any_of(names(var_labels))) %>% 
-  tbl_summary(
-    label = unname(var_labels[names(.)]),
-    statistic = list(
-      all_continuous() ~ "{mean} ({p25}-{p75})",
-      all_categorical() ~ "{n} ({p}%)"
-    ),
-    digits = all_continuous() ~ 1
-  )
+make_table_and_export <- function(df, labels, outname_stub, tbl_by = NULL){
+  tab <- df %>% 
+    dplyr::select(any_of(names(labels))) %>% 
+    tbl_summary(
+      label = unname(labels[names(.)]),
+      by = tbl_by,
+      statistic = list(
+        all_continuous() ~ "{mean} ({p25}-{p75})",
+        all_categorical() ~ "{n} ({p}%)"
+      ),
+      digits = all_continuous() ~ 1
+    )
+  
+  tab %>%
+    as_gt() %>%
+    gt::gtsave(
+      filename = paste0(outname_stub, "_description.html"),
+      path = fs::path(output_dir_tab)
+    )
+  
+  raw_stats <- tab$meta_data %>%
+    select(var_label, df_stats) %>%
+    unnest(df_stats)
+  
+  raw_stats_redacted_catgorical <- raw_stats %>%
+    filter(!is.na(n)) %>% 
+    mutate(
+      n=redact_and_round(n, threshold),
+      N=redact_and_round(N, threshold),
+      p=round(100*n/N,1),
+      N_miss = redact_and_round(N_miss, threshold),
+      N_obs = redact_and_round(N_obs, threshold),
+      p_miss = round(100*N_miss/N_obs,1),
+      N_nonmiss = redact_and_round(N_nonmiss, threshold),
+      p_nonmiss = round(100*N_nonmiss/N_obs,1),
+      var_label = factor(var_label, levels=map_chr(labels[-c(1)], ~last(as.character(.)))),
+      variable_levels = replace_na(as.character(variable_levels), "")
+    ) %>% 
+    mutate(prettyN = as.character(prettyNum(formatC(n, digits = 1, format = "f"), 
+                                            big.mark = ",", preserve.width = "none",
+                                            drop0trailing = TRUE)),
+           stat = paste0(prettyN, " (",p,"%)")) 
+  
+  if(!is.null(tbl_by)){
+    raw_stats_redacted_catgorical <- raw_stats_redacted_catgorical %>% 
+      dplyr::select(variable = var_label, 
+                    level = variable_levels, 
+                    stat,
+                    by) %>% 
+      pivot_wider(names_from = by, values_from = stat)
+  }else{
+    raw_stats_redacted_catgorical <- raw_stats_redacted_catgorical %>% 
+      dplyr::select(variable = var_label, 
+                    level = variable_levels, 
+                    stat) 
+  }
+  
+  raw_stats_redacted_numeric <- raw_stats %>% 
+    filter(is.na(n)) %>% 
+    mutate_if(is.numeric, ~prettyNum(formatC(., digits = 1, format = "f"), 
+                                     big.mark = ",", preserve.width = "none",
+                                     drop0trailing = TRUE)) %>% 
+    mutate(stat = paste0(mean, " (", p25, "-", p75,")")) 
+  
+  if(!is.null(tbl_by)){
+    raw_stats_redacted_numeric <- raw_stats_redacted_numeric %>% 
+      dplyr::select(variable = var_label, 
+                    level = variable_levels, 
+                    stat,
+                    by) %>% 
+      pivot_wider(names_from = by, values_from = stat)
+  }else{
+    raw_stats_redacted_numeric <- raw_stats_redacted_numeric %>% 
+      dplyr::select(variable = var_label, 
+                    level = variable_levels, 
+                    stat) 
+  }
+    
+  raw_stats_output <- raw_stats_redacted_catgorical %>% 
+    bind_rows(raw_stats_redacted_numeric)
+  
+  write_csv(raw_stats_output, paste0(here::here("output/"), outname_stub, "_data.csv"))
+}
 
-table1 %>%
-  as_gt() %>%
-  gt::gtsave(
-    filename = "tab1_baseline_description.html",
-    path = fs::path(output_dir_tab)
-  )
+make_table_and_export(df = baseline_data, labels = var_labels, outname_stub = "tab1_baseline")
 
-raw_stats <- table1$meta_data %>%
-  select(var_label, df_stats) %>%
-  unnest(df_stats)
+# table 2 - includes follow up time split by Long COVID -------------------
+fup_table <- cleaned_data %>%
+  # select only baseline variables 
+  dplyr::select(no_prev_vacc, 
+                pt_start_date, 
+                pt_end_date,
+                t,
+                lc_out,
+                lc_dx_only,
+                lc_cat,
+                fracture,
+                vaccine_dose_1_manufacturer,
+                covid_hosp_cat, 
+                covid_primary_cat, 
+                test_positive_cat,
+                test_total_cat,
+                sex,
+                age,
+                age_cat,
+                practice_nuts, 
+                imd_q5,
+                ethnicity,
+                comorbidities, 
+                care_home, 
+                highrisk_shield, 
+                lowrisk_shield) %>% 
+  # calculate year of study enrolment
+  dplyr::mutate(pt_start_year = factor(year(pt_start_date))) %>% 
+  dplyr::select(-pt_start_date,
+                -pt_end_date) %>% 
+  dplyr::select(pt_start_year, everything()) %>% 
+  mutate(lc_out = factor(lc_out, levels = 0:1, labels = c("None", "Long COVID"))) %>% 
+  # use forcats package to create an explicit "missing" value 
+  mutate_if(is.factor, ~forcats::fct_explicit_na(.))
 
-raw_stats_redacted_catgorical <- raw_stats %>%
-  filter(!is.na(n)) %>% 
-  mutate(
-    n=redact_and_round(n, threshold),
-    N=redact_and_round(N, threshold),
-    p=round(100*n/N,1),
-    N_miss = redact_and_round(N_miss, threshold),
-    N_obs = redact_and_round(N_obs, threshold),
-    p_miss = round(100*N_miss/N_obs,1),
-    N_nonmiss = redact_and_round(N_nonmiss, threshold),
-    p_nonmiss = round(100*N_nonmiss/N_obs,1),
-    var_label = factor(var_label, levels=map_chr(var_labels[-c(1)], ~last(as.character(.)))),
-    variable_levels = replace_na(as.character(variable_levels), "")
-  ) %>% 
-  mutate(prettyN = as.character(prettyNum(formatC(n, digits = 1, format = "f"), 
-                                          big.mark = ",", preserve.width = "none",
-                                          drop0trailing = TRUE)),
-         stat = paste0(prettyN, " (",p,"%)")) %>% 
-  dplyr::select(variable = var_label, 
-                level = variable_levels, 
-                stat) 
-
-raw_stats_redacted_numeric <- raw_stats %>% 
-  filter(is.na(n)) %>% 
-  mutate_if(is.numeric, ~prettyNum(formatC(., digits = 1, format = "f"), 
-                                   big.mark = ",", preserve.width = "none",
-                                   drop0trailing = TRUE)) %>% 
-  mutate(stat = paste0(mean, " (", p25, "-", p75,")")) %>% 
-  dplyr::select(variable = var_label, 
-                level = variable_levels, 
-                stat) 
-
-
-raw_stats_output <- raw_stats_redacted_catgorical %>% 
-  bind_rows(raw_stats_redacted_numeric) %>% 
-  mutate(variable = factor(variable,
-                           levels = c(
-                             "Follow-up start (year)",
-                             "Follow-up time (years)",
-                             "Age",
-                             "Age category",
-                             "Sex",
-                             "Region",
-                             "IMD (quintile)",
-                             "Ethnicity",
-                             "Comorbidities",
-                             "Resident in care home",
-                             "Shielding (high risk group)",
-                             "Shielding (Low/moderate risk group)"
-                           ))) %>%
-    arrange(variable, level)
-
-write_csv(raw_stats_output, here::here("output/table1_data.csv"))
+make_table_and_export(df = fup_table, tbl_by = 'lc_out', labels = var_labels_full, outname_stub = "tab2_fup")
 
 # vaccine-to-long COVID gaps by dose number --------------------------------
-vaccine_gaps_tab <- cleaned_data %>%
+vaccine_gaps_data <- cleaned_data %>% 
+  mutate(last_vacc_gap = last_vacc_gap*12) 
+
+vaccine_gaps_tab <- vaccine_gaps_data %>%
   group_by(no_prev_vacc, lc_dx_flag) %>% 
   summarise(avg_gap = mean(last_vacc_gap), sd_gap = sd(last_vacc_gap), .groups = "keep")
 write_csv(vaccine_gaps_tab, here("output/supplementary/vaccines_longcovid_gap.csv"))
 
 pdf(here("output/supplementary/fig_vaccines_longcovid_gap.pdf"), width = 8, height = 6)
-cleaned_data %>% 
+vaccine_gaps_data %>% 
   dplyr::select(no_prev_vacc, last_vacc_gap, lc_dx_flag) %>% 
   dplyr::filter(!is.na(lc_dx_flag)) %>% 
   ggplot(aes(x = last_vacc_gap, fill = no_prev_vacc)) +
   geom_density(alpha = 0.4) +
   facet_wrap(~lc_dx_flag, ncol = 1) +
-  labs(fill = "Number of vaccine doses", x = "Gap between vaccine and long COVID record (years)") +
+  labs(fill = "Number of vaccine doses", x = "Gap between vaccine and long COVID record (months)") +
   theme_ali()
 dev.off()
 
 pdf(here("output/supplementary/fig_vaccines_longcovid_gap_zoomed.pdf"), width = 8, height = 6)
-cleaned_data %>% 
+vaccine_gaps_data %>% 
   dplyr::select(no_prev_vacc, last_vacc_gap, lc_dx_flag) %>% 
   dplyr::filter(!is.na(lc_dx_flag)) %>% 
   ggplot(aes(x = last_vacc_gap, fill = no_prev_vacc)) +
   geom_density(alpha = 0.4) +
   facet_wrap(~lc_dx_flag, ncol = 1) +
-  xlim(c(0, 2)) +
-  labs(fill = "Number of vaccine doses", x = "Gap between vaccine and long COVID record (years)") +
+  xlim(c(6, 0)) +
+  labs(fill = "Number of vaccine doses", x = "Gap between vaccine and long COVID record (months)") +
   theme_ali()
 dev.off()
