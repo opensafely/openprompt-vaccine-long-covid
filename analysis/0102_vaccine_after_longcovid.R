4600819783e80e240c696d2c7d62c1970fb2f81d
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2023-07-03 16:12:00 +0100

    Update 0102_vaccine_after_longcovid.R

--- analysis/0102_vaccine_after_longcovid.R
+++ analysis/0102_vaccine_after_longcovid.R
@@ -10,18 +10,35 @@ source(here::here("analysis/functions/ggplot_theme.R"))
 
 # create folders to put plots in
 dir.create(here("output/figures"), showWarnings = FALSE, recursive=TRUE)
-
-lc_first <- arrow::read_parquet(here("output/clean_dataset_lc_first.gz.parquet"))
-
 output_dir_tab <- here("output/tables")
 fs::dir_create(output_dir_tab)
 
-vacc_range <- range(lc_first$no_total_vacc)
-lc_first$over2_vacc = factor(lc_first$no_total_vacc, levels = 0:vacc_range[2], labels = c("0", "1", rep("2+", vacc_range[2] - vacc_range[1] - 1)))
-lc_first_vacced <- lc_first %>% 
-  group_by(over2_vacc, sex, age_cat, ethnicity) %>% 
+
+# import data -------------------------------------------------------------
+lc_first <- arrow::read_parquet(here("output/clean_dataset_lc_first.gz.parquet"))
+clean_data_non_long_covid <- arrow::read_parquet(here("output/clean_dataset.gz.parquet")) %>% 
+  filter(lc_out == 0)
+
+# combine lc --> vaccine, and no_lc --> vaccine ---------------------------
+vaccine_data <- lc_first %>% 
+  dplyr::select(patient_id, no_total_vacc, sex, age_cat, ethnicity) %>% 
+  mutate(no_prev_vacc = cut(
+    no_total_vacc,
+    breaks = c(-Inf, 0:2, Inf),
+    labels = c(as.character(0:2), "3+")
+    ),
+    lc_out = 1
+  ) %>%  
+  bind_rows(
+    dplyr::select(clean_data_non_long_covid, patient_id, no_prev_vacc, sex, age_cat, ethnicity, lc_out)
+  )
+
+# regroup vaccines --------------------------------------------------------
+lc_first_vacced <- vaccine_data %>% 
+  mutate(lc_out = factor(lc_out)) %>% 
+  group_by(no_prev_vacc, lc_out, age_cat, ethnicity) %>% 
   summarise(n = n(), .groups = "keep") %>% 
-  group_by(sex, age_cat, ethnicity) %>% 
+  group_by(lc_out, age_cat, ethnicity) %>% 
   mutate(group_n = sum(n)) %>% 
   ungroup() %>% 
   mutate(pct_vacc = round((n/group_n)*100, 2))
@@ -30,13 +47,13 @@ lc_first_vacced <- lc_first %>%
 write.csv(lc_first_vacced, here::here("output/lc_first_vaccination.csv"))
 
 pdf(here("output/figures/fig6_vaccine_after_lc.pdf"), width = 8, height = 6)
-ggplot(lc_first_vacced, aes(x = over2_vacc, y = pct_vacc, fill = sex, colour = sex)) + 
+ggplot(lc_first_vacced, aes(x = no_prev_vacc, y = pct_vacc, fill = lc_out, colour = lc_out)) + 
   geom_col(position = position_dodge(width = 0.2), alpha = 0.6) +
   coord_flip() +
   labs(y = "# vaccines received",
        x = "%",
-       fill = "Sex",
-       colour = "Sex"
+       fill = "long COVID",
+       colour = "long COVID"
        ) +
   facet_grid(age_cat~ethnicity) +
   theme_ali()
