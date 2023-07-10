3f5e154bb6ca86ea7ac5d18993039242349a319e
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2023-07-10 15:27:00 +0100

    add count histograms

--- analysis/040_poisson_checks.R
+++ analysis/040_poisson_checks.R
@@ -14,13 +14,13 @@ clean_data <- arrow::read_parquet(here::here("output/clean_dataset.gz.parquet"))
 
 ## import time_updated_data - all long COVID
 time_data_lc_all <- arrow::read_parquet(here::here("output/timeupdate_dataset_lc_all.gz.parquet")) %>% 
-  dplyr::select(patient_id, starts_with("pt"), age, age_cat, sex, lc_out = out, no_prev_vacc = vaccines) %>% 
+  dplyr::select(patient_id, starts_with("pt"), t, age, age_cat, sex, lc_out = out, no_prev_vacc = vaccines, t_vacc_mrna) %>% 
   mutate(lc_dx_only = lc_out) %>% 
   left_join(dplyr::select(clean_data, patient_id, region), by = "patient_id")
 
 ## import time_updated_data - long COVID diagnoses
 time_data_lc_dx <- arrow::read_parquet(here::here("output/timeupdate_dataset_lc_dx.gz.parquet")) %>% 
-  dplyr::select(patient_id, starts_with("pt"), age, age_cat, sex, lc_dx_only = out, no_prev_vacc = vaccines) %>% 
+  dplyr::select(patient_id, starts_with("pt"), t, age, age_cat, sex, lc_dx_only = out, no_prev_vacc = vaccines, t_vacc_mrna) %>% 
   mutate(lc_out = lc_dx_only) %>% 
   left_join(dplyr::select(clean_data, patient_id, region), clean_data, by = "patient_id")
 
@@ -94,3 +94,45 @@ check_poisson <- function(data_in, stub_name){
 check_poisson(data_in = clean_data, stub_name = "clean_data_poisson_checks")
 check_poisson(data_in = time_data_lc_all, stub_name = "time_data_lc_first_poisson_checks")
 check_poisson(data_in = time_data_lc_dx, stub_name = "time_data_lc_dx_poisson_checks")
+
+
+# plot distribution of counts ---------------------------------------------
+## convert to data.table and rename the outcome var as `out`
+dt_poisson_lc_all <- data.table::setDT(time_data_lc_all)
+colnames(dt_poisson_lc_all)[colnames(dt_poisson_lc_all)=="lc_out"] <- "out"
+
+dt_poisson_lc_dx <- data.table::setDT(time_data_lc_dx)
+colnames(dt_poisson_lc_dx)[colnames(dt_poisson_lc_dx)=="lc_dx_only"] <- "out"
+
+# little function to draw a histogram
+poisson_hist <- function(DT, stratifier){
+  dt_name <- deparse(substitute(DT))
+  
+  col <- ifelse(stringr::str_detect(dt_name, "lc_dx"), 
+                rgb(0,0.2,0.5, alpha = 0.4),
+                rgb(0.8,0.5,0.2, alpha = 0.4))
+  
+  dt_summ <- dt_poisson[, .(out=sum(out), t=sum(t)),
+     by = c(stratifier, "age_cat", "sex", "region")]
+  
+  hist(dt_summ$out, breaks = 20,
+       main = eval(stratifier),
+       xlab = "No. with outcome",
+       col = col)
+}
+
+pdf(here::here("output/data_properties/poisson_checks/poisson_hist.pdf"), 12, 5)
+par(mfrow=c(2,4))
+  # any long covid code
+  poisson_hist(dt_poisson_lc_all, "age_cat")
+  legend("topright", c("any long COVID"), col = rgb(0.8,0.5,0.2, alpha = 1), lty = 1, lwd = 2, bty = "n")
+  poisson_hist(dt_poisson_lc_all, "sex")
+  poisson_hist(dt_poisson_lc_all, "no_prev_vacc")
+  poisson_hist(dt_poisson_lc_all, "t_vacc_mrna")
+  # lc diagnoses only
+  poisson_hist(dt_poisson_lc_dx, "age_cat")
+  legend("topright", c("long COVID diagnoses"), col = rgb(0,0.2,0.5, alpha = 1), lty = 1, lwd = 2, bty = "n")
+  poisson_hist(dt_poisson_lc_dx, "sex")
+  poisson_hist(dt_poisson_lc_dx, "no_prev_vacc")
+  poisson_hist(dt_poisson_lc_dx, "t_vacc_mrna")
+dev.off()
