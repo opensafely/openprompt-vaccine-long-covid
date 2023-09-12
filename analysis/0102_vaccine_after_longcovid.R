607315206155ea5bb85b58e48cdb6429ef5f816c
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2023-09-12 15:02:28 +0100

    change vaccine after long covid to coverage

--- analysis/0102_vaccine_after_longcovid.R
+++ analysis/0102_vaccine_after_longcovid.R
@@ -21,12 +21,12 @@ clean_data_non_long_covid <- arrow::read_parquet(here("output/clean_dataset.gz.p
 
 # combine lc --> vaccine, and no_lc --> vaccine ---------------------------
 vaccine_data <- lc_first %>% 
-  dplyr::select(patient_id, no_total_vacc, first_lc, starts_with("pt_"), sex, age_cat) %>% 
+  dplyr::select(patient_id, no_post_lc_vacc, first_lc, starts_with("pt_"), sex, age_cat) %>% 
   mutate(
     # make the lc date the start of obs. period
     pt_start_date = first_lc,
     no_prev_vacc = cut(
-      no_total_vacc,
+      no_post_lc_vacc,
       breaks = c(-Inf, 0:2, Inf),
       labels = c(as.character(0:2), "3+")
     ), 
@@ -39,25 +39,29 @@ vaccine_data <- lc_first %>%
     pyrs = (pt_start_date %--% pt_end_date) / dyears(1)
   )
 
-# regroup vaccines --------------------------------------------------------
+
+# calculate vaccine coverage on 31st Jan 2023 ------------------------------
 lc_first_vacced <- vaccine_data %>% 
+  filter(pt_end_date >= as.Date("2023-01-31")) %>% 
   mutate(lc_out = factor(lc_out)) %>% 
   group_by(no_prev_vacc, lc_out, age_cat) %>% 
-  summarise(n = n(), pyrs = sum(pyrs), .groups = "keep") %>% 
+  summarise(n = n(), .groups = "keep") %>% 
+  group_by(lc_out, age_cat) %>% 
+  mutate(group_size = sum(n)) %>% 
   ungroup() %>% 
   mutate(
-    n = redact_and_round(n, redact_threshold = 10),
-    vacc_rate = round((n*1e5)/pyrs, 2)
+    n = n,#redact_and_round(n, redact_threshold = 10),
+    vacc_prev = round((n*100)/group_size, 2)
     )
 
 write.csv(lc_first_vacced, here::here("output/lc_first_vaccination.csv"))
 
 pdf(here("output/figures/fig6_vaccine_after_lc.pdf"), width = 6, height = 6)
-ggplot(lc_first_vacced, aes(x = no_prev_vacc, y = vacc_rate, fill = lc_out, colour = lc_out)) + 
+ggplot(lc_first_vacced, aes(x = no_prev_vacc, y = vacc_prev, fill = lc_out, colour = lc_out)) + 
   geom_col(position = position_dodge(width = 0.2), alpha = 0.6) +
   coord_flip() +
   labs(x = "# vaccines received",
-       y = "Vaccination rate (per 100,000 person-years",
+       y = "Vaccination coverage as of Jan 2023",
        fill = "long COVID",
        colour = "long COVID"
        ) +
